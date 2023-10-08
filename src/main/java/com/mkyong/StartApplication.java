c5ddde00ae138c85dd7bdaaadcdfbeda6b10eadc
Bandita Banerjee <bandita.cse@gmail.com>
2023-10-08 21:14:37 +0530

    Fourth commit

--- src/main/java/com/mkyong/StartApplication.java
+++ src/main/java/com/mkyong/StartApplication.java
@@ -6,10 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-public class StartApplication{
+public class StartApplication extends SpringBootServletInitializer{
 
     private static final Logger log = LoggerFactory.getLogger(StartApplication.class);
 
